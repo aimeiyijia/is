facd6eac987939ce6ca9196f6fb6b33b0793d47c
aimeiyijia <1456723200@qq.com>
2022-04-29 19:59:46 +0800

    feat: 增加类型判断

--- src/type.ts
+++ src/type.ts
@@ -1,5 +1,8 @@
 import { cached } from './shared'
 
+var objProto = Object.prototype
+var hasOwn = objProto.hasOwnProperty
+
 const _toString = Object.prototype.toString
 const typeFactory = cached(function (originType: string) {
   return (value: any): boolean => {
@@ -16,25 +19,75 @@ export function isString(value: any) {
 }
 
 export function isBoolean(value: any) {
-  typeFactory('Boolean')(value)
+  return typeFactory('Boolean')(value)
 }
 
 export function isNull(value: any) {
-  typeFactory('Null')(value)
+  return typeFactory('Null')(value)
 }
 
 export function isSymbol(value: any) {
-  typeFactory('Symbol')(value)
-}
-
-export function isFunction(value: any) {
-  typeFactory('Function')(value)
+  return typeFactory('Symbol')(value)
 }
 
 export function isObject(value: any) {
-  typeFactory('Object')(value)
+  return typeFactory('Object')(value)
 }
 
 export function isArray(value: any) {
-  typeFactory('Array')(value)
+  return typeFactory('Array')(value)
+}
+
+export function isArrayLike(value: any) {
+  return (
+    !!value &&
+    !isBoolean(value) &&
+    hasOwn.call(value, 'length') &&
+    isFinite(value.length) &&
+    isNumber(value.length) &&
+    value.length >= 0
+  )
+}
+
+export function isArguments(value: any) {
+  return typeFactory('Arguments')(value)
+}
+
+export function isDate(value: any) {
+  return typeFactory('Date')(value)
+}
+
+// 是否是一个错误对象
+export function isError(value: any) {
+  return typeFactory('Error')(value)
+}
+
+export function isFunction(value: any) {
+  var isAlert = typeof window !== 'undefined' && value === window.alert
+  if (isAlert) {
+    return true
+  }
+  return (
+    typeFactory('Function')(value) ||
+    typeFactory('GeneratorFunction')(value) ||
+    typeFactory('AsyncFunction')(value)
+  )
+}
+
+export function isEmpty(value: any) {
+  if (isArray(value) || isArguments(value) || isString(value)) {
+    return value.length === 0
+  }
+
+  if (isObject(value)) {
+    let key
+    for (key in value) {
+      if (hasOwn.call(value, key)) {
+        return false
+      }
+    }
+    return true
+  }
+
+  return !value
 }
